ba78d6c371697f4112164dc17f5daa57c97192ba
Adarsh Maurya <adarshmaurya500@gmail.com>
2023-11-19 13:57:35 +0530

    initiated

---
+++ main.lua
@@ -0,0 +1,71 @@
+--[[
+    CS50x Final Project
+    Space shooter Remake
+
+    Author: Adarsh Maurya
+    adarshmaurya500@gmail.com
+    
+    A classic space shooter game using a free art pack.
+
+    Art pack:
+    https://opengameart.org/content/kenney-16x16
+
+    Music:
+    https://freesound.org/people/Sirkoto51/sounds/393818/
+]]
+love.graphics.setDefaultFilter('nearest', 'nearest')
+require 'src/Dependencies'
+
+function love.load()
+    love.graphics.setFont(gFonts['medium'])
+    love.window.setTitle('Space Shooter')
+
+    math.randomseed(os.time())
+    
+    push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
+        fullscreen = false,
+        vsync = true,
+        resizable = true,
+        canvas = false
+    })
+
+    gStateMachine = StateMachine {
+        ['start'] = function() return StartState() end,
+        ['play'] = function() return PlayState() end
+    }
+    gStateMachine:change('start')
+
+    gSounds['music']:setLooping(true)
+    -- gSounds['music']:setVolume(0.5)
+    gSounds['music']:play()
+
+    love.keyboard.keysPressed = {}
+end
+
+function love.resize(w, h)
+    push:resize(w, h)
+end
+
+function love.keypressed(key)
+    if key == 'escape' then
+        love.event.quit()
+    end
+
+    love.keyboard.keysPressed[key] = true
+end
+
+function love.keyboard.wasPressed(key)
+    return love.keyboard.keysPressed[key]
+end
+
+function love.update(dt)
+    gStateMachine:update(dt)
+
+    love.keyboard.keysPressed = {}
+end
+
+function love.draw()
+    push:start()
+    gStateMachine:render()
+    push:finish()
+end
